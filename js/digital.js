22f1e7a198f74dd8ce7b276e68f1e3d6ea7bf647
Ikram Qabiyev <ikram20011127@gmail.com>
2017-08-02 21:46:34 +0400

    completed the top part of Digital Main Page

---
+++ js/digital.js
@@ -0,0 +1,115 @@
+var condition=false;
+$(".fa-bars").click(function(){
+    if (condition==false) {
+        $(".mainDrop").slideDown()
+        condition=true;
+    }
+    else {
+        $(".mainDrop").slideUp()
+        condition=false;
+    }
+})
+// active click on navigation
+$(".navBarChoices ul li a").click(function(elem) {
+    $(".navBarChoices ul li a").removeClass("activateLink");
+    $(this).addClass("activateLink");
+})
+
+// event listener for services button
+var serCond=false
+var $services = $(".nav .navBar .navBarChoices > ul > li:nth-child(1)");
+var $serDropDown = $(".nav .navBar .navBarChoices > ul > li:nth-child(1) ul")
+$($services).click(function(){
+    if (serCond==false) {
+        $($serDropDown).slideDown()
+        serCond=true;
+    }
+    else {
+        $($serDropDown).slideUp()
+        serCond=false;
+    }
+})
+
+// event listener for services button in the main dropdown
+var mainSerCond=false
+var $mainServices = $(".mainDrop > ul > li:nth-child(1)");
+var $mainSerDropDown = $(".mainDrop > ul > ul");
+$($mainServices).click(function(){
+    if (mainSerCond==false) {
+        $($mainSerDropDown).slideDown()
+        mainSerCond=true;
+    }
+    else {
+        $($mainSerDropDown).slideUp()
+        mainSerCond=false;
+    }
+})
+
+// animation for top slider in the digital page
+
+var $leftBtn = $("#digital .topMovingSlidesFrame .arrowLeft");
+var $rightBtn = $("#digital .topMovingSlidesFrame .arrowRight");
+var topSliderImgArr = ["img/digital/slider-1.jpg","img/digital/slider-2.jpg"];
+var $animatedImg = $("#digital .topMovingSlidesFrame img");
+var $h1TopItem = $("#digital .topMovingSlidesFrame .h1");
+var $h2TopItem = $("#digital .topMovingSlidesFrame .h2");
+var $quoteTopSlides = $("#digital .topMovingSlidesFrame .overBtnsTop")
+// event for left button
+var indexTop = 0;
+$($leftBtn).on("click",function(){
+    $($h1TopItem).slideUp(1000);
+    $($h2TopItem).slideUp(1000);
+    $($quoteTopSlides).slideUp(1000);
+    if(indexTop == 1) {
+        $($animatedImg).attr("src",topSliderImgArr[0]);
+        indexTop = 0;
+    }
+    else {
+        $($animatedImg).attr("src",topSliderImgArr[1]);
+        indexTop = 1;
+    }
+    $($h1TopItem).slideDown(1000);
+    $($h2TopItem).slideDown(1000);
+    $($quoteTopSlides).slideDown(1000);
+    var curWidth=120;
+    function sizeAnim() 
+    {
+        if (curWidth<=150) 
+        {
+            curWidth+=0.035;
+            console.log(curWidth);
+            $($animatedImg).css("width",curWidth + "%");
+        }
+    }
+    setInterval(sizeAnim,10);
+})
+
+//event for right button 
+
+$($rightBtn).on("click",function(){
+    $($h1TopItem).slideUp(1000);
+    $($h2TopItem).slideUp(1000);
+    $($quoteTopSlides).slideUp(1000);
+    if(indexTop == 1) {
+        $($animatedImg).attr("src",topSliderImgArr[0]);
+        indexTop = 0;
+    }
+    else {
+        $($animatedImg).attr("src",topSliderImgArr[1]);
+        indexTop = 1;
+    }
+    $($h1TopItem).slideDown(1000);
+    $($h2TopItem).slideDown(1000);
+    $($quoteTopSlides).slideDown(1000);
+    var curWidth=120;
+    function sizeAnim() 
+    {
+        if (curWidth<=150) 
+        {
+            curWidth+=0.035;
+            console.log(curWidth);
+            $($animatedImg).css("width",curWidth + "%");
+        }
+    }
+    setInterval(sizeAnim,10);
+})
